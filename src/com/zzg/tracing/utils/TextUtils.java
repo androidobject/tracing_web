06531e56c137a3bc38e70ebe159b080667ca1423
zhao <zzg19910406@163.com>
2018-11-07 13:35:35 +0800

    代码优化、、、

--- src/com/zzg/tracing/utils/TextUtils.java
+++ src/com/zzg/tracing/utils/TextUtils.java
@@ -2,6 +2,11 @@ package com.zzg.tracing.utils;
 
 public class TextUtils {
 
+    /**
+     * 判断是否为null
+     * @param o
+     * @return
+     */
     public static boolean isEmpty(String o) {
 
         if (o == null || "".equals(o)) {
